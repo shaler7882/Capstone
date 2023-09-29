ed1d7604e027b5b190c2175a3c7591aa1f5ce039
shaler7882 <shawnhaler@yahoo.com>
2023-09-28 17:06:40 -0700

    working on ecto

--- lib/hangman_web/controllers/page_controller.ex
+++ lib/hangman_web/controllers/page_controller.ex
@@ -6,10 +6,4 @@ defmodule HangmanWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
-
-  def game(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :game, layout: false)
-  end
 end
--- lib/hangman_web/live/game_live.ex
+++ lib/hangman_web/live/game_live.ex
@@ -1,41 +1,90 @@
 defmodule HangmanWeb.GameLive do
   use HangmanWeb, :live_view
+  import Ecto.Query, warn: false
+  alias Hangman.Repo
+
+  alias Hangman.Accounts.{User, UserToken, UserNotifier}
 
   def mount(params, session, socket) do
     list_of_words = [
-      "elixir"
-      # "strings",
-      # "booleans",
-      # "atoms",
-      # "tuples",
-      # "lists",
-      # "maps",
-      # "functions"
+      "elixir",
+      "strings",
+      "booleans",
+      "atoms",
+      "tuples",
+      "lists",
+      "maps",
+      "functions"
     ]
 
-    winning_word = Enum.random(list_of_words)
-    {:ok, assign(socket, winning_word: winning_word, guesses: ["e", "i"], lives: 6)}
+    winning_word = String.downcase(Faker.Vehicle.make())
+    {:ok, assign(socket, winning_word: winning_word, guesses: [], lives: 6, result: false, body_img: "/images/post.jpg", high_score: 0)}
   end
 
   def render(assigns) do
     ~H"""
-    <%= for char <- String.graphemes(@winning_word) do %>
-      <%= if char in @guesses, do: char, else: "_" %>
-    <% end %>
-
-    <.form for={%{}} phx-submit="enter_guess">
-      <.input name="guess" value="" />
-    </.form>
-
-    <%= for char <- String.graphemes("ABCDEFGHIJKLMNOPQRSTUVWXYZ") do %>
-    <.button phx-click="enter_guess" phx-value-guess={String.downcase(char)}> <%= char %> </.button>
-    <% end %>
+    <div>
+      <%= if @result do %>
+        <.link href="/game">
+          <.button class="win">WINNER<br />Play Again</.button>
+        </.link>
+      <% end %>
+    </div>
+    <div>
+      <%= if @lives == 0 do %>
+        <.link href="/game">
+          <.button class="win">LOSER<br />Play Again</.button>
+        </.link>
+      <% end %>
+    </div>
+    <div class="lives">Lives Left = <%= @lives %></div>
+    <div class="high">Score <%= @high_score %></div>
+    <img class="pic" src={@body_img}>
+    <div class="word">
+      <%= for char <- String.graphemes(@winning_word) do %>
+        <%= if char in @guesses, do: char, else: "_" %>
+      <% end %>
+    </div>
+    <div class="guess">
+      <%= for char <- String.graphemes("ABCDEFGHIJKLMNOPQRSTUVWXYZ") do %>
+        <%= if String.downcase(char) in @guesses do %>
+          <.button></.button>
+        <% else %>
+          <.button phx-click="enter_guess" phx-value-guess={String.downcase(char)}>
+            <%= char %>
+          </.button>
+        <% end %>
+      <% end %>
+    </div>
     """
   end
 
   def handle_event("enter_guess", unsigned_params, socket) do
-    IO.inspect(unsigned_params)
     %{"guess" => guess} = unsigned_params
-    {:noreply, assign(socket, guesses: [guess | socket.assigns.guesses])}
+    guesses = [guess | socket.assigns.guesses]
+
+    minus_life =
+      Enum.member?(String.graphemes(socket.assigns.winning_word), guess)
+
+    lives = if minus_life, do: socket.assigns.lives, else: socket.assigns.lives - 1
+
+    high_score = if minus_life, do: socket.assigns.high_score + 100, else: socket.assigns.high_score
+
+    body_part =
+      case lives do
+        6 -> "/images/post.jpg"
+        5 -> "/images/head.jpg"
+        4 -> "/images/body.jpg"
+        3 -> "/images/rightarm.jpg"
+        2 -> "/images/leftarm.jpg"
+        1 -> "/images/rightleg.jpg"
+        0 -> "/images/leftleg.jpg"
+        _ -> ""
+      end
+
+    is_winner =
+      Enum.all?(String.graphemes(socket.assigns.winning_word), fn each -> each in guesses end)
+    
+    {:noreply, assign(socket, guesses: guesses, result: is_winner, lives: lives, body_img: body_part, high_score: high_score)}
   end
 end
